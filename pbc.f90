5e865e2c4d2aa29f7863f676bff50f9cd5728bf4
Jaume Garcia <jgarciatheatre@gmail.com>
2021-03-01 16:41:28 +0100

    Modificacio boundary conditions

--- pbc.f90
+++ pbc.f90
@@ -4,7 +4,8 @@ implicit none
 contains
 
 subroutine PBC1(L,x)
-! Subrutina que calcula les boundary conditions aplicant la minimum image convention.
+! Subrutina que calcula les boundary conditions aplicant la minimum image convention
+! i si una partícula surt de la caixa, entra per l'altre costat.
 ! Argument d'entrada: tamany de la caixa L
 ! Argument de sortida: posició de la partícula, x.
 ! Adreçada al càlcul de la força LJ.
@@ -19,20 +20,3 @@ endif
 return 
 endsubroutine 
 
-subroutine PBC2(L,x)
-! Subrutina que calcula les boundary conditions de manera que si una partícula surt de la caixa
-! entra per l'altre costat.
-! Argument d'entrada: tamany de la caixa L
-! Argument de sortida: posició de la partícula, x.
-! Adreçada al càlcul de les posicions de les partícules.
-implicit none
-double precision x,L
-if (x > L) then 
-	x = x - L
-endif    
-if (x < 0.d0) then
-   x = x + L
-endif 
-endsubroutine 
-
-end module boundary
