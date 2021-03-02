e284f80dbfd77bd139552acda87b937507e5b6cd
Edgar Alvarez Galera <ealvarga23@alumnes.ub.edu>
2021-03-02 14:52:15 +0100

    Integration 02/03/2020 -v2

--- Integration.f90
+++ Integration.f90
@@ -20,15 +20,16 @@
 !	rnew --> New positions of the particles (after implementing the step).
 !	vnew --> New velocities of the particles.
 !	Fnew --> New forces between particles.
-	USE boundary, forces
+	use boundary
+	use forces
 	IMPLICIT NONE
 	INTEGER, intent(in) :: N
-	REAL*8, intent(in) dt, L, rcut
+	REAL*8, intent(in) :: dt, L, rcut
 	REAL*8, intent(inout) :: r(3,N), rnew(3,N)
 	REAL*8, intent(inout) :: v(3,N), vnew(3,N)
 	REAL*8, intent(inout) :: F(3,N), Fnew(3,N)
 	INTEGER i, j
-	REAL*8, intent(out) :: pot
+	REAL*8 pot
 	
 
 
@@ -46,25 +47,26 @@
 	return	
 	END SUBROUTINE
 
-C------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+!------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	
-	SUBROUTINE Integrate(Nsteps,Npart,T,dt,rho,rcut,L,thermostat
-     &	p0,v0,pf,vf,ff)
-	use statistics, forces
+	SUBROUTINE Integrate(Nsteps,Npart,T,dt,rho,rcut,L,thermostat, &
+     	r0,v0,pf,vf,ff)
+	use statistics
+	use forces
 	IMPLICIT NONE
 	INTEGER, intent(in) :: Nsteps, Npart
 	REAL*8, intent(in) :: T, dt, rho, L, rcut
 	LOGICAL, intent(in) :: thermostat
 	REAL*8, intent(in) :: r0(3,Npart), v0(3,Npart)
 	REAL*8, intent(out) :: pf(3,Npart), vf(3,Npart), ff(3,Npart)
-	REAL*8 pos(3,Npart), v(3,Npart), forc(3,Npart)
-	REAL*8 newp(3,Npart), newv(3,Npart), newf(3,Npart)
+	REAL*8 pos(3,Npart), vel(3,Npart), forc(3,Npart)
+	REAL*8 np(3,Npart), nv(3,Npart), nf(3,Npart)
 	INTEGER i, j
-	REAL*8 time, KE, PE, totalE, Tinst, pressure
+	REAL*8 time, KE, PE, totalE, Tinst, pressio
 	
 	
 1  	FORMAT(A1,2X,3(F14.8,2X))
-2  	FORMAT(5(F14.8,2X))	
+2  	FORMAT(6(F14.8,2X))	
 	
 	open(14,file="Positions.xyz")
 	open(15,file="Thermodynamics.dat")
@@ -85,14 +87,14 @@ C-------------------------------------------------------------------------------
 	call kinetic(Npart,nv,KE)
 	call insttemp(Npart,KE,T)
 	totalE = totalenergy(PE,KE)
-	call pressure(Npart,L,rho,pos,forc,T,pressure)
+	call pressure(Npart,L,rho,pos,forc,T,pressio)
 	
-	write(15,2) time, KE, PE, totalE, Tinst, pressure !Write the values in "thermodynamics.dat"		
+	write(15,2) time, KE, PE, totalE, Tinst, pressio !Write the values in "thermodynamics.dat"		
 	
 	
 	do i=1,Nsteps
 		time = dble(i)*dt
-		call Velocity_Verlet(Npart,dt,L,pos,vel,forc,np,nv,nf)
+		call Velocity_Verlet(Npart,dt,L,rcut,pos,vel,forc,np,nv,nf)
 		if (thermostat .eqv. .true.) call Andersen(Npart,T,nv)
 		
 ! Write the new positions to in XYZ format (trajectories):		
@@ -110,9 +112,9 @@ C-------------------------------------------------------------------------------
 		call kinetic(Npart,nv,KE)
 		call insttemp(Npart,KE,T)
 		totalE = totalenergy(PE,KE)
-		call pressure(Npart,L,rho,pos,forc,T,pressure)
+		call pressure(Npart,L,rho,pos,forc,T,pressio)
 
-		write(15,2) time, KE, PE, totalE, Tinst, pressure !Write the values in "thermodynamics.dat"		
+		write(15,2) time, KE, PE, totalE, Tinst, pressio !Write the values in "thermodynamics.dat"		
 		
 	enddo	
 	
@@ -126,7 +128,7 @@ C-------------------------------------------------------------------------------
 	return
 	END SUBROUTINE Integrate
 	
-C------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+!------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 	SUBROUTINE ANDERSEN(N,T,v)
       IMPLICIT NONE
@@ -149,3 +151,17 @@ C-------------------------------------------------------------------------------
       
       return
       END SUBROUTINE
+  
+!------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	SUBROUTINE BOXMULLER(SIGMA,X1,X2,XOUT1,XOUT2)
+      IMPLICIT NONE
+      double precision PI, sigma, x1, x2, xout1, xout2
+      PI = 4d0*datan(1d0)
+      
+      XOUT1=sigma*dsqrt(-2d0*(dlog(1d0-x1)))*dcos(2d0*PI*x2)
+      XOUT2=sigma*dsqrt(-2d0*(dlog(1d0-x1)))*dsin(2d0*PI*x2)
+       
+      END SUBROUTINE BOXMULLER
+     
+     endmodule integration
