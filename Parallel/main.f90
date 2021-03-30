6a8d956e652653677ac4d57207d48013eae43581
Jaume Garcia <jgarciatheatre@gmail.com>
2021-03-30 17:28:08 +0200

    Prova paralel

--- Parallel/main.f90
+++ Parallel/main.f90
@@ -5,10 +5,13 @@ program main
 use Initialize
 use Integration
 use reader 
+use parallel
 
 implicit none 
 
 real*8 L,rcut
+real*8 t_ini,t_final
+integer master
 
 LOGICAL true ! Paràmetre lògic que afegeix el termostat
 
@@ -19,10 +22,19 @@ REAL*8, allocatable :: vf(:,:)
 REAL*8, allocatable :: ff(:,:)
 REAL*8 temp,density,timestep,sigma,epsilon,mass
 integer Natoms,Nsteps,Nradial
+integer numproc
+
+
+call MPI_INIT(ierror)
+call MPI_COMM_RANK(MPI_COMM_WORLD,taskid,ierror)
+call MPI_COMM_SIZE(MPI_COMM_WORLD,numproc,ierror)
+t_ini = MPI_WTIME()
+master = 0
 
 ! Lectura del fitxer amb les dades necessàries per la simulació
 call read_files(temp,density,timestep,sigma,epsilon,mass,Natoms,Nsteps,Nradial)
 
+call assignacio(Natoms,numproc)
 
 allocate(pos0(3,Natoms))
 allocate(vel0(3,Natoms))
@@ -35,6 +47,10 @@ call initial(Natoms,density,temp,pos0,vel0,L)
 
 rcut= L/2.d0 ! Cut-off
 
+! Compartim les posicions i velocitats inicials amb tots els processadors
+call MPI_BCAST(pos0, Natoms*3, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, IERROR)
+call MPI_BCAST(vel0, Natoms*3, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, IERROR)
+
 ! Integrem les equacions del moviment i escrivim els resultats en fitxers
 call Integrate(Nsteps,Natoms,Nradial,temp,timestep,density,rcut,L,sigma,true,pos0, &
 vel0,pf,vf,ff)
@@ -44,6 +60,13 @@ deallocate(vel0)
 deallocate(pf)
 deallocate(vf)
 deallocate(ff)
+
+t_final = MPI_WTIME()
+IF (taskid==0) then
+    print*, "Número de processadors = ",numproc
+    print*, "Temps de simulació = ",endtime-starttime
+END IF
+call MPI_FINALIZE(IERROR)
 return
 
 end program main
